b28168e7d6ba0d00490c8f1a4b32345925b1f8cc
RizOrt218 <rizhiafalen@gmail.com>
2015-12-25 15:07:09 -1000

    created fn & get elem tree

--- christmasTree.js
+++ christmasTree.js
@@ -1,17 +1,7 @@
+var tree = document.getElementById('tree');
+var branches = '*0';
 
-var trunk = ['||'];
+function formBranches() {
 
-function buildTree(branches) {
-  for (var i = 0; i < branches; i++) {
-    if ( i < branches ){
-      trunk.push("000");
-      return;
-    }
-    if( trunk < branches ) {
-      trunk.push(("000")*2);
-      return trunk;
-    }
-  }
-buildTree(10);
 }
-console.log(trunk);
+formBranches();
